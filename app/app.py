e5544fc4c9c05f9397f10ca2b12c8425faa8871b
Adrian Marin <adrian.marin@brex.io>
2019-01-23 11:38:09 +0100

    Added env on Docker and used from flask

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
+import os
+default = os.environ.get('name')
 
 
 @app.route('/')
-def hello_whale():
-    return "Hello, Docker"
+def hello_world():
+    return "Hello {}".format(default)
+@app.route('/<name>')
+def hello_name(name=default):
+    return "Hello {}".format(name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
